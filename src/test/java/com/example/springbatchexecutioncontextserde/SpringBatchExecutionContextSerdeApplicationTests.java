8c5a3066e970606ebb28b1a9eb33964c650856f3
Olivier Hermans <oli4hermans@gmail.com>
2022-01-15 14:50:15 +0100

    project setup

---
+++ src/test/java/com/example/springbatchexecutioncontextserde/SpringBatchExecutionContextSerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbatchexecutioncontextserde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchExecutionContextSerdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
